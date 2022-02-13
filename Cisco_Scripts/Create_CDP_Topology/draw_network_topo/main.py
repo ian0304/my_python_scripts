5039a5a926013b89280e6faef93fb62cfa6a965d
Ian Yang <ianyoung0304@gmail.com>
2022-02-13 21:52:14 +0800

    Put cdp_to_dict func in main.py

--- Cisco_Scripts/Create_CDP_Topology/draw_network_topo/main.py
+++ Cisco_Scripts/Create_CDP_Topology/draw_network_topo/main.py
@@ -1,9 +1,12 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_command
+from draw_network_graph import draw_topology
+
 def cdp_to_dict(cdp_list):
+    # get cdp result from Nornir_Scrapli
     import re
-    from pprint import pp
     result_dict = {}
     topology_dict = {}
-    pp(cdp_list)
     for lines in cdp_list:
         loc_dev_regex = re.compile('\S+')
         regex = re.compile('(\S+?)[\s\.][\s+\S+][\s\n]\s+(\S+\s?\S+)\s+\d+\s+[rCDBHIMPRTS ]{5}\s+(.*)')
@@ -16,3 +19,17 @@ def cdp_to_dict(cdp_list):
                 break
             topology_dict[key] = value
     return topology_dict 
+
+def main():
+    nr = InitNornir(config_file="config.yaml")
+    result = nr.run(task=send_command, command="show cdp neighbor | begin Device ID")
+    #Change cdp result from string to list
+    result_list = []
+    for i in result:
+        #Add local device name in the begining of device result
+        result_list.append(f"{i}\n{str(result[i][0])}")
+    topology_dict = cdp_to_dict(result_list)
+    draw_topology(topology_dict)
+
+if __name__ == '__main__':
+    main()
