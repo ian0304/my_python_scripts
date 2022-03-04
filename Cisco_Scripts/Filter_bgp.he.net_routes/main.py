cd73c48e17e8ed718e92b0637808d9b75d48b847
Ian Yang <ianyoung0304@gmail.com>
2022-03-05 02:35:07 +0800

    first commit

---
+++ Cisco_Scripts/Filter_bgp.he.net_routes/main.py
@@ -0,0 +1,29 @@
+#filter dup subnet for https://bgp.he.net/
+import re
+import ipaddress
+import copy
+from pprint import pp
+
+net_regex = re.compile('(\d{1,3}\.){3}\d{1,3}/\d{1,2}')
+net = set()
+with open('file.txt') as f:
+    for line in f:
+        net_result = net_regex.search(line)
+        if bool(net_result) == True:
+            net.add(ipaddress.ip_network(net_result.group()))
+
+new_net = copy.copy(net)
+
+while bool(net) == True:
+    a = net.pop()
+    for b in net:
+        if a.supernet_of(b):
+            new_net.remove(b)
+
+sorted_net = sorted(new_net)
+write_file = []
+for line in sorted_net:
+    write_file.append(str(line)+'\n')
+
+with open('sorted.txt', 'w') as f:
+    f.writelines(write_file)
