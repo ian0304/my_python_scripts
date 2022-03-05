96d32b6fe6b8290e9e1121275f07aa962450c1c2
Ian Yang <ianyoung0304@gmail.com>
2022-03-06 02:02:26 +0800

    Change module from ipaddress to netaddr

--- Cisco_Scripts/Filter_bgp.he.net_routes/main.py
+++ Cisco_Scripts/Filter_bgp.he.net_routes/main.py
@@ -1,30 +1,20 @@
 #filter dup subnet for https://bgp.he.net/
 import re
-import ipaddress
-import copy
-from pprint import pp
+import netaddr
+from netaddr import IPNetwork
 
 net_regex = re.compile('(\d{1,3}\.){3}\d{1,3}/\d{1,2}')
-net = set()
+net = []
 with open('file.txt') as f:
     for line in f:
         net_result = net_regex.search(line)
         if bool(net_result) == True:
-            net.add(ipaddress.ip_network(net_result.group()))
+            net.append(IPNetwork(net_result.group()))
 
-new_net = copy.copy(net)
+net = netaddr.cidr_merge(net)
 
-while bool(net) == True:
-    a = net.pop()
-    for b in net:
-        if a.supernet_of(b):
-            new_net.remove(b)
-        elif b.supernet_of(a):
-            new_net.remove(a)
-
-sorted_net = sorted(new_net)
 write_file = []
-for line in sorted_net:
+for line in net:
     write_file.append(str(line)+'\n')
 
 with open('sorted.txt', 'w') as f:
