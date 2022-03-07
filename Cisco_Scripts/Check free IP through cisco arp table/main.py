c982553851266ee418f58e992db00eace95eacad
Ian Yang <ianyoung0304@gmail.com>
2022-03-07 23:42:02 +0800

    first commit

---
+++ Cisco_Scripts/Check free IP through cisco arp table/main.py
@@ -0,0 +1,22 @@
+from netaddr import IPAddress
+import netaddr
+import re
+
+
+ip_regex = re.compile('(((\d+){1,3}\.){3}(\d+){1,3})')
+
+'''About the IP Network can be optimized by using Nornir to get it under cisco device interface config '''
+arp_subnet = netaddr.IPNetwork('192.168.21.0/24')
+arp_subnet_list = list(arp_subnet.iter_hosts())
+
+with open('arp_table.txt') as f:
+    for line in f:
+        ip_result = ip_regex.search(line)
+        if bool(ip_result) == True:
+            used_ip = netaddr.IPAddress(ip_result.group())
+            arp_subnet_list.remove(used_ip)
+            
+
+with open('avalible_ip.txt', 'w') as f:
+    for ip in arp_subnet_list:
+        f.write(f'{str(ip)}\n')
