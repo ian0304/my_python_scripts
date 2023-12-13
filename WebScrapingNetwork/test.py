921030ce2178248e91ede465cb5b25e30a129af3
Ian Yang <ianyoung0304@gmail.com>
2023-12-13 10:27:50 +0800

    first test py seleninmwire

--- WebScrapingNetwork/test.py
+++ WebScrapingNetwork/test.py
@@ -1,1 +1,22 @@
-print('hello')
+from seleniumwire import webdriver  # Import from seleniumwire
+
+# Create a new instance of the Chrome driver
+driver = webdriver.Chrome()
+
+# Go to the Google home page
+driver.get('https://www.baidu.com')
+
+#create a file to save URL
+with open('url.txt', 'w') as f:
+
+# Access requests via the `requests` attribute
+    for request in driver.requests:
+        if request.response:
+            f.writelines(request.url+'\n')
+            '''
+            print(
+                request.url,
+                request.response.status_code,
+                request.response.headers['Content-Type']
+            )
+            '''
