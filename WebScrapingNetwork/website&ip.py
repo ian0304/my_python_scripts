4b7564630826bfd5434cacecbd2083f91e67c599
Ian Yang <ianyoung0304@gmail.com>
2023-12-14 00:24:34 +0800

    website&ip

---
+++ WebScrapingNetwork/website&ip.py
@@ -0,0 +1,31 @@
+from seleniumwire import webdriver  # Import from seleniumwire
+import pandas as pd
+import dns.resolver
+import re
+
+
+# Create a new instance of the Chrome driver
+driver = webdriver.Chrome()
+
+# Go to the Google home page
+driver.get('https://www.163.com')
+
+data = {}
+
+# Access requests via the `requests` attribute
+for request in driver.requests:
+    if request.response:
+        pattern = re.compile('https?://([^/]*)/')
+        website = pattern.search(request.url)
+        ip = dns.resolver.resolve(website.group(1), 'A')
+        ips = str()
+        for ipval in ip:
+            ips+=(ipval.to_text() + '\n')
+        data[website.group(1)]=ips
+
+# convert dictionary to a pandas DataFrame
+web_ip = pd.DataFrame(list(data.items()), columns=['Website', 'IP'])
+web_ip['IP'] = web_ip['IP'].astype(str)
+# write the DataFrame to an Excel file
+web_ip.to_excel('website&ip.xlsx', index=False)
+        
