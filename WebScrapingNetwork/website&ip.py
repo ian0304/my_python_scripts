b6688f5dc715473ad0b1e6497300680a6a1d57e2
Ian Yang <ianyoung0304@gmail.com>
2023-12-14 20:29:21 +0800

    code update

--- WebScrapingNetwork/website&ip.py
+++ WebScrapingNetwork/website&ip.py
@@ -4,35 +4,51 @@ import dns.resolver
 import re
 import easygui
 
-# Show an input box for the user to enter the URL
-url = easygui.enterbox(msg='Enter the URL:', title='URL Input')
+def get_webpage_data(url):
+    """
+    This function takes a URL as input, visits the webpage using Selenium,
+    and returns a dictionary mapping websites to their IP addresses.
+    """
 
-# Check if https:// or http:// is present in the URL, if not add it
-if not re.match('(?:http|ftp|https)://', url):
-    url = 'https://{}'.format(url)
+    # Check if https:// or http:// is present in the URL, if not add it
+    if not re.match('(?:http|ftp|https)://', url):
+        url = 'https://{}'.format(url)
 
-# Create a new instance of the Chrome driver
-driver = webdriver.Chrome()
+    # Create a new instance of the Chrome driver
+    driver = webdriver.Chrome()
 
-# Go to the webpage
-driver.get(url)
+    # Go to the webpage
+    driver.get(url)
 
-data = {}
+    data = {}
 
-# Access requests via the `requests` attribute
-for request in driver.requests:
-    if request.response:
-        pattern = re.compile('https?://([^/]*)/')
-        website = pattern.search(request.url)
-        ip = dns.resolver.resolve(website.group(1), 'A')
-        ips = str()
-        for ipval in ip:
-            ips+=(ipval.to_text() + '\n')
-        data[website.group(1)]=ips
+    # Access requests via the `requests` attribute
+    for request in driver.requests:
+        if request.response:
+            website = re.search('https?://([^/]*)/', request.url).group(1)
+            ip = dns.resolver.resolve(website, 'A')
+            ips = '\n'.join(ipval.to_text() for ipval in ip)
+            data[website] = ips
 
-# Convert dictionary to a pandas DataFrame
-web_ip = pd.DataFrame(list(data.items()), columns=['Website', 'IP'])
-web_ip['IP'] = web_ip['IP'].astype(str)
+    return data
 
-# Write the weburl&ip to an Excel file
-web_ip.to_excel('website&ip.xlsx', index=False)
+def save_data_to_excel(data, filename):
+    """
+    This function takes a dictionary and a filename as input,
+    and writes the data to an Excel file.
+    """
+
+    # Convert dictionary to a pandas DataFrame
+    web_ip = pd.DataFrame(list(data.items()), columns=['Website', 'IP'])
+    web_ip['IP'] = web_ip['IP'].astype(str)
+
+    # Write the weburl&ip to an Excel file
+    web_ip.to_excel(filename, index=False)
+
+if __name__ == "__main__":
+    # Show an input box for the user to enter the URL
+    url = easygui.enterbox(msg='Enter the URL:', title='URL Input')
+
+    data = get_webpage_data(url)
+
+    save_data_to_excel(data, 'website&ip.xlsx')
